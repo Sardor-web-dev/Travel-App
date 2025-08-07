788d8a4433b5ebefda7893380acb670e8a1fa0b0
Sardor-web-dev <dzamolovsardor5@gmail.com>
2025-08-07 03:35:50 +0300

    done trip generation form soon will be integrated ai

--- app/routes.ts
+++ app/routes.ts
@@ -8,6 +8,8 @@ export default [
   route("sign-in", "routes/root/sign-in.tsx"),
   layout("routes/admin/admin-layout.tsx", [
     route("dashboard", "routes/admin/dashboard.tsx"),
+    route("trips", "routes/admin/trips.tsx"),
+    route("trips/create", "routes/admin/create-trip.tsx"),
     route("all-users", "routes/admin/all-users.tsx"),
   ]),
 ] satisfies RouteConfig;
